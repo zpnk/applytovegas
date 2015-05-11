6cc1d56a2003b9fa76ef584c933def96e9343cb5
Zach K <z@zpnk.me>
2015-05-11 11:00:00 -0700

    pages controller

---
+++ controllers/pages.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  home: function(req, res) {
+    res.sendStatus(200)
+  }
+
+}
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express')
 var port    = process.env.PORT || 9765
 var server  = express()
 
+var pages = require('./controllers/pages')
+
 server.disable('x-powered-by')
 
-server.get('/', function(req, res) {
-  res.sendStatus(200)
-})
+server.get('/', pages.home)
 
 server.listen(port, function() {
   console.log('Now serving on port', port)
