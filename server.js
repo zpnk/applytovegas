593d55fe639ae421d4d0b4e6e47185fbee5d335b
Zach K <z@zpnk.me>
2015-05-12 12:00:00 -0700

    companies controller

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var port    = process.env.PORT || 9765
 var server  = express()
 
 var pages = require('./controllers/pages')
+var companies = require('./controllers/companies')
 
 server.disable('x-powered-by')
 
 server.get('/', pages.home)
+server.get('/companies', companies.new)
 
 server.listen(port, function() {
   console.log('Now serving on port', port)
