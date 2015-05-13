166bdb4abbf22f964ec2ff3f6dd5a61ec8d065cb
Zach K <z@zpnk.me>
2015-05-13 13:00:00 -0700

    candidates controller

---
+++ controllers/candidates.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  new: function(req, res) {
+    res.sendStatus(200)
+  }
+
+}
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var server  = express()
 
 var pages = require('./controllers/pages')
 var companies = require('./controllers/companies')
+var candidates = require('./controllers/candidates')
 
 server.disable('x-powered-by')
 
 server.get('/', pages.home)
 server.get('/companies', companies.new)
+server.get('/candidates', candidates.new)
 
 server.listen(port, function() {
   console.log('Now serving on port', port)
