7a4f93dd4cb12bd3f6b6869b4c53c374b4001ef9
Zach K <z@zpnk.me>
2015-05-17 13:00:00 -0700

    companies view

--- controllers/companies.js
+++ controllers/companies.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   new: function(req, res) {
-    res.sendStatus(200)
+    res.render('companies')
   }
 
 }
--- controllers/pages.js
+++ controllers/pages.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   home: function(req, res) {
-    res.render('home')
+    res.render('home', {fullHeader: true})
   }
 
 }
